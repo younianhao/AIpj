66e8b438abd057e40c16522ae1244998c2056b9f
RKStone-Y <ethanyuan2304@gmail.com>
2024-05-24 01:31:35 +0800

    Update train.sh

--- pj3/RGBTCC/train.sh
+++ pj3/RGBTCC/train.sh
@@ -1,7 +1,7 @@
 python train.py \
     --data-dir ./rgbtcc_dataset/ \
     --save-dir ./model/ \
-    --pretrained_model model_best.pth \
+    --pretrained_model ""\
     --batch-size 8 \
     --lr 1e-5 \
     --device 0
