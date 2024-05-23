8a92b97e4de2ed34f11d531268e0490f5551af9a
RKStone-Y <ethanyuan2304@gmail.com>
2024-05-24 01:54:30 +0800

    Update test.sh

--- pj3/RGBTCC/test.sh
+++ pj3/RGBTCC/test.sh
@@ -1,5 +1,5 @@
 python test.py \
-    --data-dir ../../rgbtcc_dataset/ \
+    --data-dir ../rgbtcc_dataset/ \
     --save-dir ./model \
-    --model model_best.pth \
+    --model model/0523-174323/3_ckpt.tar \
     --device 0
