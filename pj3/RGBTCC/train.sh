d80d3ebdb040d9890a6260092e119d9a5140a1ce
RKStone-Y <ethanyuan2304@gmail.com>
2024-05-24 21:52:41 +0800

    Update max epoch

--- pj3/RGBTCC/train.sh
+++ pj3/RGBTCC/train.sh
@@ -2,6 +2,7 @@ python train.py \
     --data-dir ./rgbtcc_dataset/ \
     --save-dir ./model \
     --pretrained_model ./pretrained/best_model.pth \
+    --max-epoch 150 \
     --batch-size 8 \
     --lr 1e-5 \
     --device 0
